86c6714de92581e41d135acaf6bc9caee16b649c
Syggnus <chuan.ferreira@outlook.com>
2022-07-15 11:32:41 -0300

    Desafio1
    Pergunta e Reposta

---
+++ Geração Tech Unimed - BH - Fullstack/Módulo III - Conhecendo a Linguagem de Programação JavaScript/Aula 9 - Introdução Prática ao TypeScript/desafio1.ts
@@ -0,0 +1,20 @@
+interface Funcionario {
+    codigo: number,
+    nome: String
+   }
+   
+   
+   let funcionario2: Funcionario = {
+       codigo: 9,
+       nome: "Igor"
+   }
+
+//Ou
+
+let funcionario: {codigo: number, nome: String} = {
+    codigo: 10,
+    nome: "Jhon"
+}
+
+
+
